c39690d05150e00c7484540e6a92ac5c1c4d8489
chikhang238 <58615264+chikhang238@users.noreply.github.com>
2020-12-14 23:04:17 +0700

    Add "detect on Video" to Streamlit App

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 import os
 import time
+import io
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
@@ -154,9 +155,20 @@ mask_image()
 def get_cap():
     return cv2.VideoCapture(0)
 
+@st.cache(allow_output_mutation=True)
+def get_cap_vid(location):
+    print("Loading in function", str(location))
+    video_stream = cv2.VideoCapture(str(location))
+
+    # Check if camera opened successfully
+    if (video_stream.isOpened() == False):
+        print("Error opening video  file")
+    return video_stream
+
+
 def mask_detection():
     st.title("Face mask detection")
-    activities = ["Image", "Webcam"]
+    activities = ["Image", "Webcam", "Video"]
     st.set_option('deprecation.showfileUploaderEncoding', False)
     choice = st.sidebar.selectbox("Mask Detection on?", activities)
 
@@ -235,5 +247,91 @@ def mask_detection():
             #  break
 
             frameST.image(frame, channels="BGR")
+
+    if choice == 'Video':
+        st.subheader("Detection on Video")
+        st.title("Play Uploaded File")
+
+        uploaded_file = st.file_uploader("Choose a video...", type=["mp4"])
+        temporary_location = False
+
+        if uploaded_file is not None:
+            g = io.BytesIO(uploaded_file.read())  ## BytesIO Object
+            temporary_location = "testout_simple.mp4"
+
+            with open(temporary_location, 'wb') as out:  ## Open temporary file as bytes
+                out.write(g.read())  ## Read bytes into file
+
+            # close file
+            out.close()
+        # vs = VideoStream(src=0).start()
+        # time.sleep(2.0)
+        scaling_factorx = 0.7
+        scaling_factory = 0.7
+        image_placeholder = st.empty()
+
+        if temporary_location:
+            while True:
+                cap = get_cap_vid(temporary_location)
+
+                frameST = st.empty()
+                param=st.sidebar.slider('choose your value')
+
+                ap = argparse.ArgumentParser()
+                ap.add_argument("-f", "--face", type=str,
+                    default="face_detector",
+                    help="path to face detector model directory")
+                ap.add_argument("-m", "--model", type=str,
+                    default="mask_detector.model",
+                    help="path to trained face mask detector model")
+                ap.add_argument("-c", "--confidence", type=float, default=0.5,
+                    help="minimum probability to filter weak detections")
+                args = vars(ap.parse_args())
+
+                # load our serialized face detector model from disk
+                print("[INFO] loading face detector model...")
+                prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
+                weightsPath = os.path.sep.join([args["face"],
+                    "res10_300x300_ssd_iter_140000.caffemodel"])
+                faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+                # load the face mask detector model from disk
+                print("[INFO] loading face mask detector model...")
+                maskNet = load_model(args["model"])
+
+                while True:
+                    ret, frame = cap.read()
+                    #print(frame.shape)
+                    # Stop the program if reached end of video
+                    frame = cv2.resize(frame, None, fx=scaling_factorx, fy=scaling_factory, interpolation=cv2.INTER_AREA)
+                    print("Done processing !!!")
+                    (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+                    # loop over the detected face locations and their corresponding
+                    # locations
+                    for (box, pred) in zip(locs, preds):
+                        # unpack the bounding box and predictions
+                        (startX, startY, endX, endY) = box
+                        (mask, withoutMask) = pred
+
+                        # determine the class label and color we'll use to draw
+                        # the bounding box and text
+                        label = "Mask" if mask > withoutMask else "No Mask"
+                        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+                        # include the probability in the label
+                        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+                        # display the label and bounding box rectangle on the output
+                        # frame
+                        cv2.putText(frame, label, (startX, startY - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+                    cv2.waitKey(1)
+                    # Release device
+                    #  cap.release()
+                    #  break
+
+                    frameST.image(frame, channels="BGR")
+
 
 mask_detection()
